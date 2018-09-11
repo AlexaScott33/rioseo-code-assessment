38f874d72f163708eee1ce3745a10aafc10c5b55
Alexa Scott <arscott4792@gmail.com>
2018-09-11 08:25:45 -0700

    rendering truck locator elements list

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,9 +8,28 @@ $(document).ready(function() {
     dataType: 'json'
   }).success(function (response) {
     // work with response data here
-    console.log(response);
+    // $('.truck-locator-list').html(response);
+    render(response);
+    // console.log(response);
   });
 
   // DEMO
   $('.map').attr('src', 'https://maps.googleapis.com/maps/api/staticmap?center=32.823943,-117.150259&zoom=13&scale=2&size=200x300&maptype=roadmap&format=png&visual_refresh=true&markers=size:small%7Ccolor:0xff0000%7Clabel:1%7C32.823943,-117.150259');
 });
+
+function render(res) {
+  const storeList = generateTruckLocatorList(res);
+  $('.truck-locator-list').html(storeList);
+}
+
+function generateTruckLocatorList(list) {
+    const currDate = new Date();
+    console.log(currDate);
+  const listItems = list.map(truck => `
+      <li key=${truck.id} class="truck-locator-element">
+        ${truck.name}
+      </li>`);
+  const newList = listItems.join('');
+  console.log(newList);
+  return newList;
+}
